2e4c102d84d336487287ed9c761a747b32adb861
WashingtonTrevizan <ju.trevizan@hotmail.com>
2024-09-06 09:34:36 -0300

    Adicionado lista suspensa no formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,18 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 
+const Formulario = () => {
 
-function Formulario() {
+    const time = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Desing',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -10,9 +20,13 @@ function Formulario() {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa itens ={time} label="Equipes"/>
             </form>
         </section>
     );
+
 }
+
+
 
 export default Formulario
